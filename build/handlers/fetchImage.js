dca34c2ee5da163c464fa543fb77bc29534a5ecb
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-12-01 16:11:04 -0300

    Re-optimize

--- build/handlers/fetchImage.js
+++ build/handlers/fetchImage.js
@@ -1,35 +1,14 @@
 const undici = require("undici");
 
-async function fetchFromUrl(url) {
-    try {
-        const response = await undici.fetch(url, { cache: "force-cache" });
-        return response.ok ? response : null;
-    } catch (error) {
-        console.error(`Error fetching ${url}:`, error);
-        return null;
-    }
-}
-
 async function getImageUrl(info) {
     if (!info || !info.sourceName || !info.uri) return null;
 
-    let response;
     switch (info.sourceName.toLowerCase()) {
         case "spotify":
-            response = await fetchFromUrl(`https://open.spotify.com/oembed?url=${info.uri}`);
-            if (response) {
-                const json = await response.json();
-                return json.thumbnail_url || null;
-            }
-            break;
+            return (await fetchFromUrl(`https://open.spotify.com/oembed?url=${info.uri}`))?.json().then(json => json.thumbnail_url || null);
 
         case "soundcloud":
-            response = await fetchFromUrl(`https://soundcloud.com/oembed?format=json&url=${info.uri}`);
-            if (response) {
-                const json = await response.json();
-                return json.thumbnail_url || null;
-            }
-            break;
+            return (await fetchFromUrl(`https://soundcloud.com/oembed?format=json&url=${info.uri}`))?.json().then(json => json.thumbnail_url || null);
 
         case "youtube":
             const urls = [
@@ -39,25 +18,22 @@ async function getImageUrl(info) {
                 `https://img.youtube.com/vi/${info.identifier}/default.jpg`,
             ];
 
-            const validUrl = await findValidUrl(urls);
-            return validUrl;
+            const firstValidUrl = await Promise.any(urls.map(url => fetchFromUrl(url)));
+            return firstValidUrl ? firstValidUrl.url : null;
 
         default:
             return null;
     }
 }
 
-async function findValidUrl(urls) {
-    const promises = urls.map(url => fetchFromUrl(url));
-    const responses = await Promise.all(promises);
-    const validResponses = responses.filter(response => response !== null);
-
-    if (validResponses.length > 0) {
-        const validUrl = validResponses[0].url; 
-        return validUrl;
+async function fetchFromUrl(url) {
+    try {
+        const response = await undici.fetch(url, { cache: "force-cache" });
+        return response.ok ? response : null;
+    } catch (error) {
+        console.error(`Error fetching ${url}:`, error);
+        return null;
     }
-
-    return null;
 }
 
 module.exports = { getImageUrl };
