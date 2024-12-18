9d5cb6f7dac73f1e44591c3f893dadd80e5dd2f2
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-12-18 12:42:47 -0300

    more cleaning

--- build/structures/Connection.js
+++ build/structures/Connection.js
@@ -12,7 +12,6 @@ class Connection {
 
     setServerUpdate({ endpoint, token }) {
         if (!endpoint) throw new Error("Missing 'endpoint' property in VOICE_SERVER_UPDATE");
-
         const newRegion = endpoint.split('.')[0].replace(/[0-9]/g, "");
         if (this.region !== newRegion) {
             this.updateRegion(newRegion, endpoint, token);
@@ -33,33 +32,33 @@ class Connection {
         }
     }
 
-    setStateUpdate({ session_id, channel_id, self_deaf, self_mute }) {
-        if (!channel_id || !session_id) {
+    setStateUpdate(data) {
+        if (!data.channel_id || !data.session_id) {
             this.cleanup();
             return;
         }
-
-        if (this.player.voiceChannel !== channel_id) {
-            this.player.aqua.emit("playerMove", this.player.voiceChannel, channel_id);
-            this.player.voiceChannel = channel_id;
-            this.voiceChannel = channel_id;
+        if (this.player.voiceChannel !== data.channel_id) {
+            this.player.aqua.emit("playerMove", this.player.voiceChannel, data.channel_id);
+            this.player.voiceChannel = data.channel_id;
+            this.voiceChannel = data.channel_id;
         }
-
-        this.selfDeaf = self_deaf;
-        this.selfMute = self_mute;
-        this.voice.sessionId = session_id;
+        this.selfDeaf = data.self_deaf;
+        this.selfMute = data.self_mute;
+        this.voice.sessionId = data.session_id;
     }
 
     updatePlayerVoiceData() {
         const currentTime = Date.now();
         if (currentTime - this.lastUpdateTime >= this.updateThrottle) {
             this.lastUpdateTime = currentTime;
+            const data = ({
+                voice: this.voice,
+                volume: this.player.volume,
+            })
+
             this.player.nodes.rest.updatePlayer({
                 guildId: this.player.guildId,
-                data: {
-                    voice: this.voice,
-                    volume: this.player.volume
-                }
+                data,
             });
         }
     }
@@ -70,6 +69,12 @@ class Connection {
         this.voiceChannel = null;
         this.player.destroy();
         this.player.aqua.emit("playerDestroy", this.player);
+        this.player = null;
+        this.voice = null;
+        this.region = null;
+        this.selfDeaf = null;
+        this.selfMute = null;
+        this.voiceChannel = null;
     }
 }
 
