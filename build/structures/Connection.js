2649cc7484d3f9a58911b697723be706c8768bb8
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-12-23 17:06:31 -0300

    Optimize the connections, less api calls

--- build/structures/Connection.js
+++ build/structures/Connection.js
@@ -12,15 +12,12 @@ class Connection {
 
     setServerUpdate({ endpoint, token }) {
         if (!endpoint) throw new Error("Missing 'endpoint' property in VOICE_SERVER_UPDATE");
-        const newRegion = this.extractRegion(endpoint);
+        
+        const newRegion = endpoint.split('.')[0].replace(/[0-9]/g, "");
         if (this.region !== newRegion) {
             this.updateRegion(newRegion, endpoint, token);
             this.updatePlayerVoiceData();
         }
-    }
-
-    extractRegion(endpoint) {
-        return endpoint.split('.')[0].replace(/[0-9]/g, "");
     }
 
     updateRegion(newRegion, endpoint, token) {
@@ -28,7 +25,9 @@ class Connection {
         this.region = newRegion;
         this.voice.endpoint = endpoint;
         this.voice.token = token;
+
         this.player.aqua.emit("debug", `[Player ${this.player.guildId} - CONNECTION] ${previousVoiceRegion ? `Changed Voice Region from ${previousVoiceRegion} to ${this.region}` : `Voice Server: ${this.region}`}`);
+
         if (this.player.paused) {
             this.player.pause(false);
         }
@@ -55,13 +54,17 @@ class Connection {
         const currentTime = Date.now();
         if (currentTime - this.lastUpdateTime >= this.updateThrottle) {
             this.lastUpdateTime = currentTime;
+
             const data = {
                 voice: this.voice,
                 volume: this.player.volume,
             };
+
             this.player.nodes.rest.updatePlayer({
                 guildId: this.player.guildId,
                 data,
+            }).catch(err => {
+                this.player.aqua.emit("apiError", "updatePlayer", err);
             });
         }
     }
@@ -69,13 +72,16 @@ class Connection {
     cleanup() {
         this.player.aqua.emit("playerLeave", this.player.voiceChannel);
         this.player.voiceChannel = null;
+        this.voiceChannel = null;
+
         this.player.destroy();
         this.player.aqua.emit("playerDestroy", this.player);
+
         this.player = null;
         this.voice = null;
         this.region = null;
-        this.selfDeaf = false;
-        this.selfMute = false;
+        this.selfDeaf = null;
+        this.selfMute = null;
         this.voiceChannel = null;
     }
 }
