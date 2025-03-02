a71f3b465886e9bbcfab6e81e9546f7cdef353c1
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-03-01 22:05:12 -0300

    Small changes
    - reduced overhead i think
    - faster

--- build/handlers/fetchImage.js
+++ build/handlers/fetchImage.js
@@ -1,15 +1,19 @@
 const { request } = require("undici");
+
 const sourceHandlers = new Map([
     ['spotify', uri => fetchThumbnail(`https://open.spotify.com/oembed?url=${uri}`)],
     ['youtube', identifier => fetchYouTubeThumbnail(identifier)]
 ]);
-const YOUTUBE_URL_TEMPLATE = quality => id => `https://img.youtube.com/vi/${id}/${quality}.jpg`;
+
+const YOUTUBE_URL_TEMPLATE = (quality) => (id) => `https://img.youtube.com/vi/${id}/${quality}.jpg`;
 const YOUTUBE_QUALITIES = ['maxresdefault', 'hqdefault', 'mqdefault', 'default'].map(YOUTUBE_URL_TEMPLATE);
 
 async function getImageUrl(info) {
     if (!info?.sourceName || !info?.uri) return null;
+
     const handler = sourceHandlers.get(info.sourceName.toLowerCase());
     if (!handler) return null;
+
     try {
         return await handler(info.uri);
     } catch (error) {
@@ -33,15 +37,12 @@ async function fetchThumbnail(url) {
 }
 
 async function fetchYouTubeThumbnail(identifier) {
-    try {
-        const thumbnail = await Promise.race(
-            YOUTUBE_QUALITIES.map(urlFunc => fetchThumbnail(urlFunc(identifier)))
-        );
-        return thumbnail || null;
-    } catch (error) {
-        console.error('No valid YouTube thumbnail found:', error);
+    return Promise.race(
+        YOUTUBE_QUALITIES.map(urlFunc => fetchThumbnail(urlFunc(identifier)))
+    ).catch(() => {
+        console.error('No valid YouTube thumbnail found.');
         return null;
-    }
+    });
 }
 
 module.exports = { getImageUrl };
