ccae009eab5fc7d30cd70f46d16c1b8d6ce74edd
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-04-06 11:18:04 -0300

    Rewrite the connection manager

--- build/structures/Connection.js
+++ build/structures/Connection.js
@@ -2,40 +2,44 @@
 
 class Connection {
     constructor(player) {
-        this.playerRef = new WeakRef(player);
+        this.player = player;
         
         this.sessionId = null;
         this.endpoint = null;
         this.token = null;
-        
         this.region = null;
-        this.selfDeaf = false;
-        this.selfMute = false;
+        
         this.voiceChannel = player.voiceChannel;
         this.guildId = player.guildId;
-        
         this.aqua = player.aqua;
         this.nodes = player.nodes;
+        
+        this.selfDeaf = false;
+        this.selfMute = false;
+        
+        this.hasDebugListeners = this.aqua.listenerCount('debug') > 0;
     }
 
     setServerUpdate(data) {
-        if (!data?.endpoint) return;
+        if (!data || !data.endpoint) return;
         
         const { endpoint, token } = data;
-        const dotIndex = endpoint.indexOf('.');
         
+        const dotIndex = endpoint.indexOf('.');
         if (dotIndex === -1) return;
+        
         const newRegion = endpoint.substring(0, dotIndex);
         
         if (this.region !== newRegion) {
-            const prevRegion = this.region;
-            [this.endpoint, this.token, this.region] = [endpoint, token, newRegion];
+            this.endpoint = endpoint;
+            this.token = token;
+            this.region = newRegion;
             
-            if (this.aqua.listenerCount('debug')) {
+            if (this.hasDebugListeners) {
                 this.aqua.emit(
                     "debug",
                     `[Player ${this.guildId} - CONNECTION] Voice Server: ${
-                        prevRegion ? `Changed from ${prevRegion} to ${newRegion}` : newRegion
+                        this.region ? `Changed from ${this.region} to ${newRegion}` : newRegion
                     }`
                 );
             }
@@ -45,10 +49,12 @@ class Connection {
     }
 
     setStateUpdate(data) {
-        const { channel_id, session_id, self_deaf, self_mute } = data || {};
+        if (!data) return this.player?.destroy();
+        
+        const { channel_id, session_id, self_deaf, self_mute } = data;
         
         if (!channel_id || !session_id) {
-            this.playerRef.deref()?.destroy();
+            this.player?.destroy();
             return;
         }
         
@@ -57,36 +63,35 @@ class Connection {
             this.voiceChannel = channel_id;
         }
         
-        this.selfDeaf = !!self_deaf;
-        this.selfMute = !!self_mute;
+        this.selfDeaf = Boolean(self_deaf);
+        this.selfMute = Boolean(self_mute);
         this.sessionId = session_id;
     }
 
-    async _updatePlayerVoiceData() {
-        const player = this.playerRef.deref();
-        if (!player) return;
+    _updatePlayerVoiceData() {
+        if (!this.player) return;
 
-        try {
-            await this.nodes.rest.updatePlayer({
-                guildId: this.guildId,
-                data: { 
-                    voice: {
-                        sessionId: this.sessionId,
-                        endpoint: this.endpoint,
-                        token: this.token
-                    }, 
-                    volume: player.volume 
-                }
-            });
-        } catch (error) {
-            if (this.aqua.listenerCount('apiError')) {
+        const voiceData = {
+            sessionId: this.sessionId,
+            endpoint: this.endpoint,
+            token: this.token
+        };
+
+        this.nodes.rest.updatePlayer({
+            guildId: this.guildId,
+            data: { 
+                voice: voiceData, 
+                volume: this.player.volume 
+            }
+        }).catch(error => {
+            if (this.aqua.listenerCount('apiError') > 0) {
                 this.aqua.emit("apiError", "updatePlayer", { 
                     error,
                     guildId: this.guildId,
-                    voiceData: { ...this }
+                    voiceData
                 });
             }
-        }
+        });
     }
 }
 
