3634df0e55c8f7f5d61dcaf3c1c51898ed7b1b61
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-05-30 16:14:24 -0300

    Update Connection.js

--- build/structures/Connection.js
+++ build/structures/Connection.js
@@ -3,68 +3,65 @@
 class Connection {
     constructor(player) {
         this.player = player;
-        
-        this.sessionId = null;
-        this.endpoint = null;
-        this.token = null;
-        this.region = null;
-        
         this.voiceChannel = player.voiceChannel;
         this.guildId = player.guildId;
         this.aqua = player.aqua;
         this.nodes = player.nodes;
-        
+
+        this.sessionId = null;
+        this.endpoint = null;
+        this.token = null;
+        this.region = null;
         this.selfDeaf = false;
         this.selfMute = false;
-        
-        this.hasDebugListeners = this.aqua.listenerCount('debug') > 0;
+
     }
 
     setServerUpdate(data) {
         if (!data || !data.endpoint) return;
-        
+
         const { endpoint, token } = data;
-        
-        const dotIndex = endpoint.indexOf('.');
-        if (dotIndex === -1) return;
-        
-        const newRegion = endpoint.substring(0, dotIndex);
-        
+
+        const [newRegion] = endpoint.split('.');
+        if (!newRegion) return;
+
         if (this.region !== newRegion) {
             this.endpoint = endpoint;
             this.token = token;
             this.region = newRegion;
-            
-            if (this.hasDebugListeners) {
+
                 this.aqua.emit(
                     "debug",
                     `[Player ${this.guildId} - CONNECTION] Voice Server: ${
                         this.region ? `Changed from ${this.region} to ${newRegion}` : newRegion
                     }`
                 );
-            }
             
+
             this._updatePlayerVoiceData();
         }
     }
 
     setStateUpdate(data) {
-        if (!data) return this.player?.destroy();
-        
+        if (!data) {
+            this.player?.destroy();
+            return;
+        }
+
         const { channel_id, session_id, self_deaf, self_mute } = data;
-        
+
         if (!channel_id || !session_id) {
             this.player?.destroy();
             return;
         }
-        
+
         if (this.voiceChannel !== channel_id) {
             this.aqua.emit("playerMove", this.voiceChannel, channel_id);
             this.voiceChannel = channel_id;
         }
-        
-        this.selfDeaf = Boolean(self_deaf);
-        this.selfMute = Boolean(self_mute);
+
+        this.selfDeaf = !!self_deaf;
+        this.selfMute = !!self_mute;
         this.sessionId = session_id;
     }
 
@@ -76,22 +73,23 @@ class Connection {
             endpoint: this.endpoint,
             token: this.token
         };
-
-        this.nodes.rest.updatePlayer({
-            guildId: this.guildId,
-            data: { 
-                voice: voiceData, 
-                volume: this.player.volume 
-            }
-        }).catch(error => {
+        try {
+            this.nodes.rest.updatePlayer({
+                guildId: this.guildId,
+                data: {
+                    voice: voiceData,
+                    volume: this.player.volume
+                }
+            });
+        } catch (error) {
             if (this.aqua.listenerCount('apiError') > 0) {
-                this.aqua.emit("apiError", "updatePlayer", { 
+                this.aqua.emit("apiError", "updatePlayer", {
                     error,
                     guildId: this.guildId,
                     voiceData
                 });
             }
-        });
+        }
     }
 }
 
